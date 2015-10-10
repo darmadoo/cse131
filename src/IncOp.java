3570c4aafbc7f120822daa96f4aa2f71d82afbdc
Daisy Aurora <daisy.aurora93@gmail.com>
2015-10-09 17:52:07 -0700

    Phase1Check2 files, not done yet

---
+++ src/IncOp.java
@@ -0,0 +1,28 @@
+/**
+ * Created by clapichacu on 10/9/15.
+ */
+public class IncOp extends UnaryOp {
+
+    @Override
+    STO checkOperands(STO a, STO b) {
+        Type aType = a.getType();
+        return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "++"));
+    }
+
+    STO checkOperands(STO a) {
+        Type aType = a.getType();
+        if (!(aType instanceof NumericType)) {
+            // error when one of them is not numeric
+            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "++"));
+        } else if (aType instanceof IntType) {
+            // Int++ = Int
+            // return ExprSTO of int types
+            return new ExprSTO(a.getName() + "++" , new IntType("int", 4));
+        } else {
+            // Float++ = Float
+            // return ExprSTO of float type
+            return new ExprSTO(a.getName() + "++", new IntType("int", 4));
+        }
+    }
+
+}
