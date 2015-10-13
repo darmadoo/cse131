c54409351dab257b39dc849ff106177cd1febf3f
daisy-aurora <daisy.aurora93@gmail.com>
2015-10-13 11:36:10 -0700

    Update DecOp.java

--- src/DecOp.java
+++ src/DecOp.java
@@ -4,23 +4,23 @@ public class DecOp extends UnaryOp {
     STO checkOperands(STO a) {
         Type aType = a.getType();
 
+        if (!(aType instanceof NumericType)) {
+            // error when one of them is not numeric
+            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "--"));
+        }
         //check if it's modifiable L value
         if(a.getIsAddressable() && a.getIsModifiable()) {
-            if (!(aType instanceof NumericType)) {
-                // error when one of them is not numeric
-                return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "--"));
-            } else if (aType instanceof IntType) {
+            if (aType instanceof IntType) {
                 // Int++ = Int
                 // return ExprSTO of int types
                 return new ExprSTO(a.getName() + "--", new IntType("int", 4));
             } else {
                 // Float++ = Float
                 // return ExprSTO of float type
-                return new ExprSTO(a.getName() + "--", new IntType("int", 4));
+                return new ExprSTO(a.getName() + "--", new FloatType("float", 4));
             }
         }
-        //else return error message
         else
-            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval,  "++"));
+            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval, "--"));
     }
 }
